32cd4d0a50e835cc4cfc52a2c5c5c0ec085e91c2
npaudyal321 <naween321@gmail.com>
2022-12-25 09:07:51 +0545

    basic numpy array operations

---
+++ numpy/array_operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1, 2, 3])  # One dimensional array
+print(a[2])
+
+a = np.array([[1, 2], [3, 4]])  # two-dimensional array
+print(a.ndim)  # gives the array dimension
+print(a.size)
+print(a.shape)
